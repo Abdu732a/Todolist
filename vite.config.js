d11ff612ed861779c97de72b4fc3ff72eba62ce0
abduz732a <abduz732a@gmail.com>
2025-09-02 00:13:56 +0300

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Todolist/", // <--- ADD THIS LINE
+  base: "/Todolist/", // Make sure this line is correct
 });
